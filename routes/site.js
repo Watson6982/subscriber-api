b8a4e64829f6e140218d3436434fe9994cddfe93
Watson Joshua <Joshua.Watson@cambridgeshire.gov.uk>
2017-07-07 13:59:06 +0100

    add key value loop

--- routes/site.js
+++ routes/site.js
@@ -6,9 +6,16 @@ var Message = require('../models/message');
 
 // home page
 router.get('/', (req, res) => {
-    Message.find(function (err, messages) {
+    Message.find(function (err, results) {
 		if (err) return console.error(err);
-		res.render('index', {data: messages});
+		var messageArray = [];
+		results.forEach(function(item) {
+			var x = JSON.parse(item.message);
+			console.log(typeof x);
+			console.log(x.Lyric);
+			messageArray.push(x);
+		}, this);
+		res.render('index', {data: messageArray});
 	});
 });
 
